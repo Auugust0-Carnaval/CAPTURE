4de8c7f4b9e7224e6849f810beb0b4ba302faff6
Augusto <augustooliveixa@gmail.com>
2023-05-28 01:37:58 -0300

    Quase finalizando a navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Nav.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCookieBite } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+
 
 function Nav(){
     const [showNavbar, setShowNavbar] = useState(false);
@@ -8,21 +16,41 @@ function Nav(){
         setShowNavbar(!showNavbar);
     };
 
+
+
+    const [isNavVisible,setIsNavVisible] = useState(false);
+
+    useEffect(() => {
+        setIsNavVisible(true);
+    }, []);
+
     return (
         <div>
-            <button className="btn btn-outline-primary" onClick={handleClick}>CAPTURE</button>
+            <button className="btn btn-outline-primary" onClick={handleClick}>
+                <FontAwesomeIcon icon={faTwitter} className="Icon" />
+                 CAPTURE
+            </button>
             {showNavbar && (
                 <nav className="vertical-navbar">
-                    <ul className="list-group">
+                    <ul className="list-group btn-success">
                         <li className="list-group-item">
-                            <a href="#">FEED</a>
+                            <a href="#">
+                                <FontAwesomeIcon icon={faCookieBite} className="Icon" /> FEED
+                            </a>
                         </li>
                         <li className="list-group-item">
-                            <a href="#">NOTIFY</a>
+                            <a href="#">
+                                <FontAwesomeIcon icon={faBell} className="Icon" /> NOTIFY
+                            </a>
                         </li>
                         <li className="list-group-item">
-                            <a href="#">PROFILE</a>
+                            <a href="#">
+                                <FontAwesomeIcon icon={faUser} className="Icon" /> PROFILE
+                            </a>
                         </li>
+
+                        <a href="#" className="btn btn-outline-primary" id="more">
+                            <FontAwesomeIcon icon={faEllipsisV } className="Icon"/>                            </a>
                     </ul>
                 </nav>
             )}
