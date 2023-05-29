31bff51642a2959e08854090788df89b667a4833
Augusto <augustooliveixa@gmail.com>
2023-05-28 23:38:45 -0300

    Adicionando component Photo.js

---
+++ src/Photo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Photo(){
+
+    return(
+
+        <div className="card">
+            <img src='public/logo192.png' className="card-img-top" alt=''></img>
+            <div className="card-body">
+                <h5 className="card-title"></h5>
+                <p className="card-text"></p>
+            </div>
+        </div>
+        
+
+    )
+}
+
+
+export default Photo;
