03e7e4c063ba978f85b9a260a428c7a73d1699fc
Augusto <augustooliveixa@gmail.com>
2023-05-29 19:51:08 -0300

    Buscando dados do backend e usando UseEffect para ver alteracao de estado dos dados

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
-import React,{useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Nav from './Nav';
 import Photo from './Photo';
 import 'semantic-ui-css/semantic.min.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/photos');
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error('Erro ao obter dados do backend:', error);
+    return [];
+  }
+};
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    async function fetchDataAsync() {
+      const data = await fetchData();
+      setPhotos(data);
+    }
+    fetchDataAsync();
+  }, []);
+
+  const bufferToImageUrl = (buffer) => {
+    const arrayBuffer = new Uint8Array(buffer.data);
+    const blob = new Blob([arrayBuffer], { type: 'image/jpeg' });
+    const urlCreator = window.URL || window.webkitURL;
+    return urlCreator.createObjectURL(blob);
+  };
+
   return (
     <div className='all'>
       <div className='navbar'>
         <Nav/>
       </div>
-      <div className='conteudo'>
-        <Photo
-          ImgSrc={'https://th.bing.com/th/id/OIP.eTVndpHCHWfCB5qzx6EQBQHaEK?w=301&h=180&c=7&r=0&o=5&pid=1.7'}
-          title={'Adveture Time'}
-          description={'My favorite cartoon'}/>
+      <div className='container'>
+      <div className='row'>
+        {photos.map((photo) => (
+          <div key={photo.id} className='col-md-6'>
+            <Photo
+              ImgSrc={bufferToImageUrl(photo.Img)}
+              title={photo.title}
+              description={photo.description}
+            />
+          </div>
+        ))}
       </div>
+    </div>
     </div>
   );
 }
