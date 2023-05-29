a54b6fe2e7cd9e117cb3cde00aaa88d7505095a6
Augusto <augustooliveixa@gmail.com>
2023-05-29 02:12:33 -0300

    criando card e fazendo interpulacao com parametros de entrada

--- src/Photo.js
+++ src/Photo.js
@@ -1,18 +1,55 @@
 import React from "react";
+import './Photo.css'
 
-function Photo(){
+function Photo({ImgSrc, title, description}){
 
     return(
-
-        <div className="card">
-            <img src='public/logo192.png' className="card-img-top" alt=''></img>
-            <div className="card-body">
-                <h5 className="card-title"></h5>
-                <p className="card-text"></p>
+        <div id="card" class="ui card">
+            <div class="image">
+                <img src={ImgSrc}></img>
+            </div>
+            <div class="content">
+                <div className="row">
+                    <div className="col-8">
+                        <strong>
+                            <p class="header">{title}</p>
+                        </strong>
+                    </div>
+                    <div className="col-4">
+                        <i id="favori" class="large heart outline icon"></i>
+                        <i id="like" class="large thumbs up outline icon"></i>
+                    </div>
+                </div>
+                <div class="description">
+                    {description}
+                </div>
             </div>
         </div>
+
+
+
+
+
+        // <div className="card">
+        //     <img src={ImgSrc} className="card-img-top" alt=''></img>
+        //     <div className="card-body">
+        //         <div className="row">
+        //             <div className="col-8">
+        //                 <h5 className="card-title">{title}</h5>
+        //             </div>
+        //             <div className="col-4">
+        //                 <i id="favori" class="large heart outline icon"></i>
+        //             </div>
+        //         </div>
+        //         <p className="card-text">{description}</p>
+        //     </div>
+        // </div>
+
+
+
         
 
+        
     )
 }
 
