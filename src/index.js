00643b4f4dbb5c8c64ba59f52273b130e645b14f
Augusto <augustooliveixa@gmail.com>
2023-05-29 02:10:55 -0300

    Subindo alteracao

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
 // faz a renderização do componente
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
