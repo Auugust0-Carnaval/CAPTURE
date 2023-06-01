d7502b7d36079ea0c7ad117d255dd423618fd5a1
Augusto <augustooliveixa@gmail.com>
2023-05-31 21:58:58 -0300

    Adicionando icons na Photo

--- src/Photo.js
+++ src/Photo.js
@@ -24,32 +24,7 @@ function Photo({ImgSrc, title, description}){
                     {description}
                 </div>
             </div>
-        </div>
-
-
-
-
-
-        // <div className="card">
-        //     <img src={ImgSrc} className="card-img-top" alt=''></img>
-        //     <div className="card-body">
-        //         <div className="row">
-        //             <div className="col-8">
-        //                 <h5 className="card-title">{title}</h5>
-        //             </div>
-        //             <div className="col-4">
-        //                 <i id="favori" class="large heart outline icon"></i>
-        //             </div>
-        //         </div>
-        //         <p className="card-text">{description}</p>
-        //     </div>
-        // </div>
-
-
-
-        
-
-        
+        </div>        
     )
 }
 
