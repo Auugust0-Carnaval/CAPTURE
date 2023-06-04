2aec1b10754d4ad13df224436f62852422b40d97
Augusto <augustooliveixa@gmail.com>
2023-06-03 22:30:27 -0300

    Algumas alteracoes na hook

--- src/Photo.js
+++ src/Photo.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import './Photo.css'
 
 function Photo({ImgSrc, title, description}){
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [isSave, SetIsSave] = useState(false);
+  
+
+    const toggleSave = () =>{
+        SetIsSave(!isSave);
+    }
+
+    const toggleFavorite = () => {
+        setIsFavorite(!isFavorite);
+      };
 
     return(
-        <div id="card" class="ui card">
-            <div class="image">
-                <img src={ImgSrc}></img>
-            </div>
-            <div class="content">
-                <div className="row">
-                    <div className="col-8">
-                        <strong>
-                            <p class="header">{title}</p>
-                        </strong>
-                    </div>
-                    <div className="col-4">
-                        <i id="favori" class="large heart outline icon"></i>
-                        <i id="like" class="large thumbs up outline icon"></i>
-                    </div>
+        <div className="all">
+            <div className="image">
+                <img class="ui medium rounded image" src={ImgSrc} />
+                <div className="text-overlay">
+                    <div className="row">
+                        <div className="col-12">
+                        <i
+                            id="favori"
+                            className={`large ${isFavorite ? 'heart ' : 'heart outline'} icon`}
+                            onClick={toggleFavorite}>
+                        </i>
+                            <i id="save" 
+                                class={`large ${isSave? 'bookmark': 'bookmark outline'} icon`}
+                                onClick={toggleSave}>
+                            </i>
+                        </div>
+                    </div> 
                 </div>
-                <div class="description">
-                    {description}
+                <div id="title">
+                    <h3 className="text">{title}</h3>
+                </div>
+                <div className="close">
+                    <i class=" large x icon"></i>
                 </div>
             </div>
-        </div>        
+        </div>  
+
+        
     )
 }
-
 
 export default Photo;
