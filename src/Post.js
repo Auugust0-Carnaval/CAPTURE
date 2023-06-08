0bd362e777e71f11ee6e1382308176f1f5dd9bef
Augusto <augustooliveixa@gmail.com>
2023-06-08 04:43:16 -0300

    Adicionando backgroud e opacity nos elementos

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,7 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import { Button, Icon } from 'semantic-ui-react';
+import './Post.css'
 
 function Post() {
   const setMessage = 'Submit your drawing';
@@ -9,6 +10,8 @@ function Post() {
 
   const [isInputText, setInputText] = useState('');
   const [isFileText, setFileText] = useState('Choose File');
+
+  const [sendForm, setForm] = useState('SUBMIT')
 
   const handleButtonClick = () => {
     fileInputRef.current.click();
@@ -29,11 +32,14 @@ function Post() {
   const handleSubmit = () => {
     console.log(`Input text: ${isInputText}`);
 
+    setForm('ENVOY'); //mudando estado do value
+
     // Construir o objeto FormData com os dados do formulário
     const formData = new FormData();
     formData.append('title', isInputText);
     formData.append('Img', fileInputRef.current.files[0]);
     formData.append('description', null);
+
 
     // Fazer a chamada HTTP POST
     axios
@@ -52,20 +58,23 @@ function Post() {
 
   return (
     <div className="setCartoon">
-        <div className="ui segment">
-            <div className="ui two column very relaxed grid">
+        <div className="ui segment" id="contentPost">
+            <div className="ui two column very relaxed grid" >
                 <div className="column">
                     <strong>
                         <h4 className="text-center text-primary">{upperCaseString}</h4>
                     </strong>
-                    <p className="text-center">Do not upload illegal photos</p>
+                    <p className="text-center text-white">Do not upload illegal photos</p>
                     <div className="ui fluid left icon input">
-                        <input
-                            type="text"
-                            placeholder="Title"
-                            value={isInputText}
-                            onChange={handleInputChange}
-                        />
+                    <input
+                      type="text"
+                      placeholder="Title"
+                      value={isInputText}
+                      onChange={handleInputChange}
+                      style={{ backgroundColor: 'transparent'}}
+                      className="transparent-input"
+                    />
+
                         <i className="users icon"></i>'
                     </div>
                     <div className="file-input-container">
@@ -74,7 +83,7 @@ function Post() {
                             style={{ display: 'none' }}
                             ref={fileInputRef}
                             onChange={handleFileChange}/>
-                        <button className="ui basic button" onClick={handleButtonClick}>
+                        <button className="btn btn-outline" onClick={handleButtonClick}>
                             <i class="file icon"></i>{isFileText}
                         </button>
                     </div>
@@ -86,10 +95,11 @@ function Post() {
                     <p></p>
                 </div>
             </div>
-            <div className="ui vertical divider">OR</div>
+            <div className="ui vertical divider text-white" >OR</div>
         </div>
-        <button id="send" class="send ui blue basic button text-center" onClick={handleSubmit} style={{ display: 'block', marginLeft: '37%' }}>
-            <i class="file icon"></i>SUBMIT
+        <button id="send" class="btn btn-outline text-center
+        " onClick={handleSubmit} style={{ display: 'block', marginLeft: '33%' }}>
+            <i class="file icon"></i>{sendForm}
         </button>
     </div>
   );
